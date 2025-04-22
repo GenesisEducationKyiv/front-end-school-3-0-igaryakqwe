1de2cfd5119211abd3af15ec0292cbc58b84dace
igaryakqwe <igor.novykov@gmail.com>
2025-04-22 16:22:46 +0300

    chore: make dialogs scrollable

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import Header from '@/components/header';
 import TracksPage from '@/features/tracks/tracks.page.tsx';
+import { ScrollArea } from '@/components/ui/scroll-area.tsx';
 
 const App = () => {
   return (
-    <div>
+    <ScrollArea className="h-screen">
       <Header />
       <TracksPage />
-    </div>
+    </ScrollArea>
   );
 };
 
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,10 @@
-'use client';
-
 import { cn } from '@/lib/utils';
 import Logo from '@/components/logo';
 
 const Header = () => {
   return (
-    <header>
-      <nav
-        className={cn(
-          'sticky z-20 w-full border-b transition-colors duration-150'
-        )}
-      >
+    <header className="sticky top-0 z-50 bg-white shadow-sm">
+      <nav className={cn('w-full border-b transition-colors duration-150')}>
         <div className="mx-auto max-w-7xl px-6">
           <div className="relative flex flex-wrap items-center justify-center gap-6 py-3 lg:gap-0 lg:py-4">
             <a
