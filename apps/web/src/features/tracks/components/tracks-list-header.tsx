9037fcc62664109f7b89b10f37037dd674e41f1a
igaryakqwe <igor.novykov@gmail.com>
2025-06-28 11:08:21 +0300

    refactor: add dynamic imports

--- apps/web/src/features/tracks/components/tracks-list-header.tsx
+++ apps/web/src/features/tracks/components/tracks-list-header.tsx
@@ -1,16 +1,23 @@
+import { lazy } from 'react';
 import { PlusIcon } from 'lucide-react';
 
-import { Button } from '@/components/ui/button.tsx';
-import DeleteManyTracksDialog from '@/features/tracks/components/delete-many-tracks-dialog.tsx';
-import SelectAllTracksButton from '@/features/tracks/components/select-all-tracks-button.tsx';
-import TrackDialog from '@/features/tracks/components/track-dialog.tsx';
+import { Button } from '@/components/ui/button';
+import TrackDialog from '@/features/tracks/components/track-dialog';
+import SelectAllTracksButton from '@/features/tracks/components/select-all-tracks-button';
+import useTracksStore from '@/features/tracks/store/use-tracks.store';
+
+const DeleteManyTracksDialog = lazy(
+  () => import('@/features/tracks/components/delete-many-tracks-dialog')
+);
 
 const TracksListHeader = () => {
+  const { isSelectMode } = useTracksStore();
+
   return (
     <div className="w-full flex gap-2 flex-wrap justify-between items-center">
       <SelectAllTracksButton />
       <div className="space-x-2">
-        <DeleteManyTracksDialog />
+        {isSelectMode && <DeleteManyTracksDialog />}
         <TrackDialog>
           <Button data-testid="create-track-button" icon={<PlusIcon />}>
             Create track
