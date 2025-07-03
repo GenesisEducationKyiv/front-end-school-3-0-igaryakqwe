0d1635536347ef1fbbf2e4590344f3fab8ef5ba0
igaryakqwe <igor.novykov@gmail.com>
2025-07-03 12:33:56 +0300

    chore: remove redundant env

--- apps/web/src/lib/socket.ts
+++ apps/web/src/lib/socket.ts
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 import { env } from '@/config/env';
 
-export const socket = io(env.BASE_API_URL, {
+export const socket = io(env.API_URL, {
   transports: ['websocket', 'polling'],
   autoConnect: false,
 });
