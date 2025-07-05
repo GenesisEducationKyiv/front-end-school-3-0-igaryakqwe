12ab32166090c1d199c907268f6c253042246604
igaryakqwe <igor.novykov@gmail.com>
2025-07-05 14:55:34 +0300

    chore: optimize track image

--- apps/web/src/features/tracks/components/track-image.tsx
+++ apps/web/src/features/tracks/components/track-image.tsx
@@ -1,13 +1,9 @@
 import { lazy } from 'react';
 
-import { Avatar, AvatarFallback } from '@/components/ui/avatar';
-import { MUSIC_ICON } from '@/constants/icons';
 import { cn } from '@/lib/utils';
 
-const AvatarImage = lazy(() =>
-  import('@/components/ui/avatar').then((module) => ({
-    default: module.AvatarImage,
-  }))
+const TrackImageFallback = lazy(
+  () => import('@/features/tracks/components/track-image-fallback')
 );
 
 interface TrackImage {
@@ -18,25 +14,20 @@ interface TrackImage {
 
 const TrackImage = ({ image, alt, className }: TrackImage) => {
   return (
-    <div className={cn('aspect-square w-full p-6', className)}>
-      <Avatar className="w-full h-full rounded-lg">
-        {image && (
-          <AvatarImage
-            src={image ?? undefined}
-            alt={alt}
-            className="object-cover w-full h-full rounded-lg"
-          />
-        )}
-        <AvatarFallback>
-          <div className="w-full h-full grid place-items-center bg-gray-100 dark:bg-neutral-800 rounded-lg">
-            <img
-              src={MUSIC_ICON}
-              alt={alt}
-              className={'object-cover w-1/3 h-auto'}
-            />
-          </div>
-        </AvatarFallback>
-      </Avatar>
+    <div
+      className={cn('aspect-square w-full p-6 h-full rounded-lg', className)}
+    >
+      {image ? (
+        <img
+          src={image}
+          alt={alt}
+          className="object-cover w-full h-full rounded-lg"
+          loading="lazy"
+          fetchPriority="high"
+        />
+      ) : (
+        <TrackImageFallback alt={alt} />
+      )}
     </div>
   );
 };
