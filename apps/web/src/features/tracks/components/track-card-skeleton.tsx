e1349c9feabecb2f0b7ea07af677c2bc26924db4
igaryakqwe <igor.novykov@gmail.com>
2025-07-13 13:00:48 +0300

    fix: adjust skeleton for new track card

--- apps/web/src/features/tracks/components/track-card-skeleton.tsx
+++ apps/web/src/features/tracks/components/track-card-skeleton.tsx
@@ -5,25 +5,25 @@ import TrackImage from '@/features/tracks/components/track-image';
 const TrackCardSkeleton = () => {
   return (
     <Card className="w-full p-0 gap-0 mx-auto overflow-hidden">
-      <TrackImage className="rounded-lg" />
+      <TrackImage />
 
-      <CardContent className="pb-6">
+      <CardContent className="p-4">
         <div className="mb-4">
-          <Skeleton className="h-6 w-3/4 mx-auto mb-2" />
+          <Skeleton className="h-6 w-3/4 mb-2" />
 
-          <div className="space-y-2 text-center">
-            <Skeleton className="h-4 w-1/2 mx-auto" />
-            <Skeleton className="h-4 w-1/2 mx-auto" />
+          <div className="space-y-2">
+            <Skeleton className="h-4 w-1/2" />
+            <Skeleton className="h-4 w-1/2" />
           </div>
 
-          <div className="flex flex-wrap items-center justify-center gap-1 mt-2">
+          <div className="flex flex-wrap gap-1 mt-2">
             <Skeleton className="h-5 w-16" />
             <Skeleton className="h-5 w-16" />
             <Skeleton className="h-5 w-16" />
           </div>
         </div>
 
-        <Skeleton className="w-full h-10 mt-4 rounded-lg" />
+        <Skeleton className="w-full h-8 mt-4 rounded-lg" />
       </CardContent>
     </Card>
   );
